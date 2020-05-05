2faeeef5c514f34e8e223c04bd81c29010e62fdc
Michael Sotir <sotirm@umich.edu>
2020-05-05 16:58:35 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,7 +2,8 @@ require "pry"
 
 def reverse_each_word(string)
   split_array = string.split(" ")
-  split_array.map{|string| string.reverse}
+  reversed_words = split_array.map{|string| string.reverse}
+  reversed_words.join
   binding.pry
 
 end
